5fecbe32f3f00d7cdf1ee5adf3589ffc2bf62c78
yhorie <yuk.horie@gmail.com>
2019-04-26 23:48:00 +0900

    app6

--- src/main.js
+++ src/main.js
@@ -32,4 +32,16 @@
     }
   });
   app4.todos.push({text: 'New item'});
+
+  new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Hello Vue.js'
+    },
+    methods: {
+      reverseMessage: function(){
+        this.message = this.message.split('').reverse().join('');
+      }
+    }
+  });
 })();
