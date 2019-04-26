8cfcfcb4b5897b9725cefbc56ddb83e59d79b6fa
yhorie <yuk.horie@gmail.com>
2019-04-26 23:43:23 +0900

    app4

--- src/main.js
+++ src/main.js
@@ -21,4 +21,15 @@
     }
   });
 
+  const app4 = new Vue({
+    el: '#app-4',
+    data: {
+      todos: [
+        {text: 'Learn JavaScript'},
+        {text: 'Learn Vue'},
+        {text: 'Build something awesome'}
+      ]
+    }
+  });
+  app4.todos.push({text: 'New item'});
 })();
